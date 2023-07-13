a770b14b6322f5af0864d0a962e33512e96f190d
guguspradita <guguspradita19@gmail.com>
2023-07-13 22:01:17 +0700

    Auth:sanctrum route api

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -22,7 +22,7 @@ class PostController extends Controller
 
     public function show($id)
     {
-        $posts = Post::with('writer:id, username')->findOrFail($id);
+        $posts = Post::with('writer:id,username')->findOrFail($id);
         return new PostDetailResource($posts);
     }
 
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,6 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('/posts/{id}', [PostController::class, 'show']);
-Route::get('/posts2/{id}', [PostController::class, 'show2']);
+Route::get('/posts', [PostController::class, 'index'])->middleware(['auth:sanctum']);
+Route::get('/posts/{id}', [PostController::class, 'show'])->middleware(['auth:sanctum']);
+Route::get('/posts2/{id}', [PostController::class, 'show2'])->middleware(['auth:sanctum']);
