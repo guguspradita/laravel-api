e78492a1d103598df602af81bccaab60d9a9528e
guguspradita <guguspradita19@gmail.com>
2023-07-13 16:34:08 +0700

    Post Controller dan Post Resource

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -18,7 +18,7 @@ class PostResource extends JsonResource
             'id' => $this->id,
             'title' => $this->title,
             'news_content' => $this->news_content,
-            'created_at' => date_format($this->created_at, 'Y-m-d H:i:s')
+            'created_at' => date_format($this->created_at, 'Y-m-d H:i:s'),
         ];
     }
 }
