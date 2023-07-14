658de3ba214dfb6d2c7a2cd766faf6bb215b1487
guguspradita <guguspradita19@gmail.com>
2023-07-14 20:44:02 +0700

    Create data menggunakan api, PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Resources\PostDetailResource;
 
 class PostController extends Controller
@@ -26,4 +27,17 @@ class PostController extends Controller
         return new PostDetailResource($posts);
     }
 
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|max:255',
+            'news_content' => 'required',
+        ]);
+     
+        // The blog post is valid...
+
+        $request['author'] = Auth::user()->id;
+        $post = Post::create($request->all());
+        return new PostDetailResource($post->loadMissing('writer:id,username'));
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\AuthenticationController;
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/logout', [AuthenticationController::class, 'logout']);
     Route::get('/me', [AuthenticationController::class, 'me']);
+    Route::post('/posts', [PostController::class, 'store']);
 });
 
 Route::get('/posts', [PostController::class, 'index']);
